d3e04fa89bbea67eb330c4e2fa670613aad9def0
Guvalif <tkskzyk0925@gmail.com>
2023-04-30 05:09:58 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+Functional Blackjack Implementation Using Ramda.js & Purify
+===============================================================================
+
+## What's This?
+
+株式会社サポーターズとの共催による技育 CAMP アカデミアにおいて、『ブラックジャックを作って学ぶ関数型プログラミングの考え方』と題して講演した際のサンプルプログラムです。
+
+See also:
+
+- [イベントページ](https://talent.supporterz.jp/events/9a3fb263-ae00-44b0-b7cd-8c38b8f7a411/)
+- [講演スライド (WIP)](#)
+
+## How to Use
+
+[ダウンロードリンク](https://github.com/Guvalif/functional-blackjack-v2/archive/master.zip) をクリックすることで、 プログラムを一式ダウンロードできます。
+
+- `npm ci` : 動作に必要となるパッケージをインストールします
+- `npm start` : ブラックジャックのプログラムを起動します
+
+## Copyright (C) 2023,
+
+- [Kazuyuki TAKASE](https://github.com/Guvalif)
+- [Chatwork Co., Ltd.](https://github.com/chatwork)
+
+This software is released under [the MIT License](http://opensource.org/licenses/mit-license.php).
---
+++ index.mjs
@@ -0,0 +1,151 @@
+/*!
+ * Copyright (C) 2023,
+ * - Kazuyuki TAKASE (https://github.com/Guvalif)
+ * - Chatwork Co., Ltd. (https://github.com/chatwork)
+ * 
+ * This software is released under the MIT License.
+ * See also: http://opensource.org/licenses/mit-license.php
+ */
+
+
+import assert from 'node:assert/strict';
+import { createInterface } from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+
+import * as R from 'ramda';
+import { Maybe } from 'purify-ts/Maybe';
+import { Either, Left, Right } from 'purify-ts/Either'
+import { EitherAsync } from 'purify-ts/EitherAsync';
+
+
+/** @typedef {'A' | 'N2' | 'N3' | 'N4' | 'N5' | 'N6' | 'N7' | 'N8' | 'N9' | 'N10' | 'J' | 'Q' | 'K'} Card */
+
+
+/** @type {Record<Card, number[]>} */
+const POINT_TABLE = {
+    'A'  : [ 1, 11 ],
+    'N2' : [ 2 ],
+    'N3' : [ 3 ],
+    'N4' : [ 4 ],
+    'N5' : [ 5 ],
+    'N6' : [ 6 ],
+    'N7' : [ 7 ],
+    'N8' : [ 8 ],
+    'N9' : [ 9 ],
+    'N10': [ 10 ],
+    'J'  : [ 10 ],
+    'Q'  : [ 10 ],
+    'K'  : [ 10 ],
+};
+
+
+/** @type {Card[]} */
+const SUIT = R.flatten(R.repeat(R.keys(POINT_TABLE), 4));
+
+
+/** @type {(_: Card) => number[]} */
+const toPoint = R.prop(R.__, POINT_TABLE);
+
+
+/** @type {(_: Card[]) => number[][]} */
+const toPoints = R.map(toPoint);
+
+
+/** @type {(_: number[]) => (_: number[]) => number[]} */
+const addA2 = R.lift(R.add);
+
+
+/** @type {(_: number[][]) => number[]} */
+const calcScores = R.reduce(addA2, [ 0 ]);
+
+
+/** @type {(_: number[]) => Maybe<number>} */
+const extractValidScore = R.pipe(
+    R.filter(R.gte(21)),
+    (xs) => Math.max(...xs),
+    Maybe.fromPredicate((x) => x !== - Infinity),
+);
+
+
+/** @type {(_: () => number) => (_: Card[]) => Card[]} */
+const shuffleSuit = (random) => (suit) => {
+    let shuffledSuit = R.clone(suit);
+
+    for (let i = suit.length - 1; i > 0; i--) {
+        const j = Math.floor(random() * (i + 1));
+
+        [ shuffledSuit[i], shuffledSuit[j] ] = [ shuffledSuit[j], shuffledSuit[i] ];
+    }
+
+    return shuffledSuit;
+};
+
+
+/** @type {(_: Card[]) => Card[]} */
+const getOddSuit = R.addIndex(R.filter)((_, i) => i % 2 !== 0);
+
+
+/** @type {(_: Card[]) => Card[]} */
+const getEvenSuit = R.addIndex(R.filter)((_, i) => i % 2 === 0);
+
+
+/** @type {(_: Card[]) => Record<'Player' | 'Dealer', Card[]>} */
+const prepareGame = R.pipe(
+    shuffleSuit(Math.random),
+    R.juxt([ getOddSuit, getEvenSuit ]),
+    R.zipObj([ 'Player', 'Dealer' ]),
+);
+
+
+/** @type {(_: number) => (_: Card[]) => Maybe<number>} */
+const hit = (turn) => R.pipe(
+    R.take(turn + 2),
+    toPoints,
+    calcScores,
+    extractValidScore,
+);
+
+
+/** @type {(_: Maybe<number>, _: string) => Either<string, string>} */
+const judge = (scoreMaybe, name) => scoreMaybe.caseOf({
+    Nothing : ()  => Left(`${name} Bust !`),
+    Just    : (x) => (x === 21) ? Left(`${name} Black Jack !`) : Right(`${name}: ${x}`),
+});
+
+
+/** @type {() => EitherAsync<string, null>} */
+const prompt = () => EitherAsync.fromPromise(async () => {
+    const io = createInterface({ input, output });
+    const result = await io.question('Do you hit ? (Enter / Others) ');
+
+    io.close();
+
+    return (result !== '') ? Left('See you !') : Right(null);
+});
+
+
+// Application Entry Point
+// ============================================================================
+
+const game = prepareGame(SUIT);
+
+/** @type {(_: number) => void} */
+const main = (turn) => {
+    assert.ok(turn >= 0);
+
+    const score = R.map(hit(turn))(game);
+    const result = R.mapObjIndexed(judge)(score);
+    const showdown = Either.sequence(R.values(result));
+
+    showdown.caseOf({
+        Left  : ()   => {},
+        Right : (xs) => console.log(`Turn ${turn} --`, xs.join(', ')),
+    });
+
+    EitherAsync.liftEither(showdown).chain(() => prompt()).caseOf({
+        Left  : (x) => console.log(x),
+        Right : ()  => main(turn + 1),
+    });
+};
+
+main(0);
